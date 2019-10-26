2d86a0d4b5513389f0229a4efcec82c86c9e9326
tariq <t.kalim.95@gmail.com>
2019-10-26 19:46:11 +0200

    more run.py, still needs refactor

--- scripts/run.py
+++ scripts/run.py
@@ -23,7 +23,7 @@ def load_and_prep(data_path):
         buckets[i] = normalize(buckets[i])
         # Add Intercept
         buckets[i] = np.column((np.ones((buckets[i].shape[0], 1)), buckets[i]))
-    return buckets, y_buckets
+    return X, buckets, y_buckets, ids
 
 def get_accuracy(train_buckets, test_buckets, test_y_buckets, weights, degree_and_lambda):
     correct_predictions = 0
@@ -50,10 +50,31 @@ def train(buckets, y_buckets, degrees, lambdas):
     return weights
 
 
-
 if __name__ == '__main__':
-    buckets, y_buckets = load_and_prep(TRAIN_DATA_PATH)
-    submission_buckets, submission_y_buckets = load_and_prep(TEST_DATA_PATH)
+    train_x, buckets, y_buckets, ids_train = load_and_prep(TRAIN_DATA_PATH)
+    submission_x, submission_buckets, submission_y_buckets, ids_submission = load_and_prep(TEST_DATA_PATH)
 
     weights = train(train_buckets=buckets, y_buckets=y_buckets, degrees=DEGREES, lambdas=LAMBDAS)
+
+    #TODO: refactor below
+    ids_array = ids_submission
+    pri_jet_num_feature = submission_x[:, 22]
+    der_mass_mmc_col_feature = submission_x[:, 0]
+    ids_array = np.column_stack((ids_array, pri_jet_num_feature))
+    ids_array = np.column_stack((ids_array, der_mass_mmc_col_feature))
+    # 
+    id_buckets = get_id_buckets(ids_array)
+    #
+    submission = predict_labels(weights[0], build_poly(submission_x_buckets[0], DEGREES[0]))
+    submission = np.column_stack((submission, id_buckets[0]))
+    for i in range(1, len(weights)):
+        degree = DEGREES[i]
+        predictions = predict_labels(weights[i], build_poly(submission_x_buckets[i], degree))
+        predictions = np.column_stack((predictions, id_buckets[i]))
+        submission = np.concatenate((submission, predictions))
+    #
+    submission = submission[submission[:, 1].argsort()]
+    submission = submission[:, 0]
+    #
+    create_csv_submission(ids_test, submission, "../data/output.csv")
     
